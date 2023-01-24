550f34e80e6008556fd608cae656e8790be128a3
Nandini <nandini.chy001@gmail.com>
2023-01-25 01:11:14 +0530

    added a new component manually & bootstrap

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({        //Decorator 
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})            
+export class ServerComponent {
+
+}
