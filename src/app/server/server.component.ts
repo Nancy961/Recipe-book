ccc93dbe379e1b478ecbc9c782dc0a47c583e666
Nandini <nandini.chy001@gmail.com>
2023-01-25 01:48:17 +0530

    multiple selectors for components

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
 
 @Component({        //Decorator 
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+    selector: 'app-server',         // tag
+    // selector: '.app-server',     // class
+    // selector: '[app-server]',    // attribute
+    templateUrl: './server.component.html',
+    styles:  [`
+        p {
+            color: dodgerblue;
+        }
+    `]
 })            
 export class ServerComponent {
 
