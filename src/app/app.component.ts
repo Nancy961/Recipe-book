b5e39316a1915c11b43ffbb68bea708eb5d7c61e
Nandini <nandini.chy001@gmail.com>
2023-01-30 12:37:59 +0530

    Data & event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h1 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
+
+  //Both ways are acceptable
   title = 'Recipe-book';
+  userName: string = 'Nandini';
 }
