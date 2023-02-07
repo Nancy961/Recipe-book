c3ff7654d6cc0c636d779efcfccbb5b64d9e6286
Nandini <nandini.chy001@gmail.com>
2023-02-08 03:18:54 +0530

    Ingredient Model + Adding to list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Potato', 3),
+    new Ingredient('Salt', 1)
+  ];
 
+  constructor() { }
+
+  ngOnInit() { }
 }
