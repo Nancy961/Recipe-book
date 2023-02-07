774ee281c536883344f272b97ac7369c74b92983
Nandini <nandini.chy001@gmail.com>
2023-02-08 02:40:27 +0530

    Adding recipe model and first dummy recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Dummy recipe', 'Dummy descr', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2019/11/samosa-recipe-500x375.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
